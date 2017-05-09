77da72f2f785eb7ccb745ce2f32a4041eb7bf17b
Etio <etiotan@gmail.com>
2017-05-08 20:29:15 -0400

    dummy contact data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import TableExampleComplex from './Components/ContactList'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-          Hello World
+
+
       </div>
     );
   }
---
+++ src/data/data.js
@@ -0,0 +1,25 @@
+//dummy contacts generated from https://mockaroo.com/
+
+const contactData = [
+    {
+        "name": "Robina",
+        "email": "rosharkey0@cafepress.com",
+        "phone_number": "174.166.174.219"
+    }, {
+        "name": "Lolly",
+        "email": "lbeavors1@washingtonpost.com",
+        "phone_number": "249.143.120.93"
+    }, {
+        "name": "Bridget",
+        "email": "bbleaden2@wisc.edu",
+        "phone_number": "189.188.253.182"
+    }, {
+        "name": "Aldo",
+        "email": "aokeevan3@digg.com",
+        "phone_number": "71.59.243.231"
+    }, {
+        "name": "Elysee",
+        "email": "ebreffitt4@taobao.com",
+        "phone_number": "96.39.93.112"
+    }
+]
