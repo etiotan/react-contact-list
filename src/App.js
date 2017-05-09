df1e0a20ec94d744f3948fc1387ab0a3707ce894
Etio <etiotan@gmail.com>
2017-05-08 20:37:08 -0400

    added materilize so it looks decent while making it, will add more styling later

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App blue-text">
 
-
+<h1>
+    Hello World
+</h1>
       </div>
     );
   }
